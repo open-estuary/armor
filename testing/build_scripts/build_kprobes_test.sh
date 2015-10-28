32929449f1c31ac3661d3ced9ac900881b735f01
Shiju Jose <shiju.jose@huawei.com>
2015-10-28 11:50:52 +0000

    changes to build kprobes test code using open-estuary/estuary/build.sh

--- testing/build_scripts/build_kprobes_test.sh
+++ testing/build_scripts/build_kprobes_test.sh
@@ -7,9 +7,12 @@
 ################################################################################
 #!/bin/bash
 
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-gnu-
+
 echo "Building the Kprobes's test code..."
 
-if [ "$1" == '' ]; then   
+if [ "$1" = '' ]; then   
     echo "Invalid parameter passed. Usage sh build_kprobes.sh <kernal build directory>"
     exit
 fi
@@ -18,8 +21,10 @@ export KPROBES_KERN_DIR=$1
 echo $KPROBES_KERN_DIR
 
 cd ../source/kprobes_test/
+
 make
 
+cd -
 echo "For testing copy binaries kprobe_test and kprobe_test.ko and script test_kprobes.sh to the rootfs"
 
 echo "Building the Kprobes's test code finished"

78534793e5f218c704758ba9760b03ff1b6a93f2
Shiju Jose <shiju.jose@huawei.com>
2015-11-25 12:45:40 +0000

    added test script for the memwatch tool

---
+++ testing/test_scripts/test_memwatch.sh
@@ -0,0 +1,36 @@
+###############################################################################
+
+# This script tests the memwatch tool
+
+#Usage:
+# $ sh test_memwatch.sh
+
+################################################################################
+#!/bin/bash
+    
+echo "Build & run test code for memwatch"
+
+cd ../source/memwatch-2.69
+
+#wget http://memwatch.sourceforge.net/memwatch-2.69.tar.gz
+#tar -xvzf memwatch-2.69.tar.gz 
+
+OLD_STR="\$(CC)"
+NEW_STR="gcc"
+sed -i -e "s/$OLD_STR/$NEW_STR/g" Makefile
+
+OLD_STR="\#error \"Hey\! Don't just compile this program, read the comments first\!\""
+NEW_STR="\/\* \#error \"Hey\! Don't just compile this program, read the comments first\!\" \*\/"
+sed -i -e "s/$OLD_STR/$NEW_STR/g" test.c 
+
+make 
+./a.out
+vi memwatch.log
+
+cd -
+
+echo "testing memwatch  tool finished"
+
+exit
+
+

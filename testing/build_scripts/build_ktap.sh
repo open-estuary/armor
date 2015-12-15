99d62f926655c113e07287b71daf2d919b5a4c31
Shiju Jose <shiju.jose@huawei.com>
2015-12-15 10:26:36 +0000

    updated build_ktap.sh

--- testing/build_scripts/build_ktap.sh
+++ testing/build_scripts/build_ktap.sh
@@ -10,6 +10,9 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 
+kernel_version=$(sudo cat $1/include/config/kernel.release)
+#echo "kernel_version=$kernel_version"
+
 if [ "$1" = '' ] || [ "$2" = '' ]; then
     echo "Invalid parameter passed. Usage sh build_ktap.sh <kernal build directory> <distribution directory>" 
     exit
@@ -24,8 +27,8 @@ sed -i "s@$OLD_STR@$NEW_STR@" Makefile
 make
 
 # copy prebuilt binary on D02 to rootfs.
-sudo cp ktap  $2/usr/local/armor/test_scripts 
-sudo cp ktapvm.ko  $2/usr/local/armor/test_scripts 
+sudo cp ktap  $2/usr/bin
+sudo cp ktapvm.ko $2/lib/modules/$kernel_version/armor
 sudo cp samples/helloworld.kp  $2/usr/local/armor/test_scripts 
 cd -
 

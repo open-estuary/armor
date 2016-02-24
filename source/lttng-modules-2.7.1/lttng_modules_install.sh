45534bfa297fce15ac8e01ce33c6ce7f5472637c
Shiju Jose <shiju.jose@huawei.com>
2016-02-24 14:13:36 +0000

    updated lttng_modules_install.sh script

--- source/lttng-modules-2.7.1/lttng_modules_install.sh
+++ source/lttng-modules-2.7.1/lttng_modules_install.sh
@@ -18,17 +18,13 @@ sudo cp $(pwd)/lttng-ring-buffer-metadata-mmap-client.ko $2/lib/modules/$kernel_
 sudo cp $(pwd)/lttng-statedump.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/lttng-tracer.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-ftrace.ko $2/lib/modules/$kernel_version/armor
-sudo cp $(pwd)/probes/lttng-kprobes.ko $2/lib/modules/$kernel_version/armor
-sudo cp $(pwd)/probes/lttng-kretprobes.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-asoc.ko $2/lib/modules/$kernel_version/armor 
 sudo cp $(pwd)/probes/lttng-probe-block.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-btrfs.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-compaction.ko $2/lib/modules/$kernel_version/armor
-sudo cp $(pwd)/probes/lttng-probe-ext3.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-ext4.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-gpio.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-irq.ko $2/lib/modules/$kernel_version/armor
-sudo cp $(pwd)/probes/lttng-probe-jbd.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-jbd2.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-kmem.ko $2/lib/modules/$kernel_version/armor
 sudo cp $(pwd)/probes/lttng-probe-kvm.ko $2/lib/modules/$kernel_version/armor
@@ -61,99 +57,91 @@ modules_dep_file=$(echo "$2/lib/modules/$kernel_version/modules.dep")
 
 #echo "modules_dep_file=$modules_dep_file" 
 
-sudo sh -c "echo armor/lttng-probe-kvm.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-kvm.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-sock.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-sock.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-asoc.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-asoc.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-ring-buffer-metadata-client.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-metadata-client.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-printk.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-printk.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-napi.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-napi.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-v4l2.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-v4l2.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
 sudo sh -c "echo armor/lttng-statedump.ko: >> $modules_dep_file" 
 
-sudo sh -c "echo armor/lttng-probe-btrfs.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-btrfs.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-ring-buffer-client-mmap-discard.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-client-mmap-discard.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-kmem.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-kmem.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-compaction.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-compaction.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-ring-buffer-client-overwrite.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko  >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-client-overwrite.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-ring-buffer-client-mmap-overwrite.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-client-mmap-overwrite.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-sunrpc.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-sunrpc.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
 sudo sh -c "echo armor/lttng-ftrace.ko: >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-signal.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-signal.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-module.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-module.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-ring-buffer-client-discard.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-client-discard.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-timer.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-timer.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
 sudo sh -c "echo armor/lttng-types.ko: >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-net.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-net.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-writeback.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-writeback.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-gpio.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-gpio.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-udp.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-udp.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-kretprobes.ko: >> $modules_dep_file"
-
-sudo sh -c "echo armor/lttng-ring-buffer-metadata-mmap-client.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-ring-buffer-metadata-mmap-client.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
 sudo sh -c "echo armor/lttng-lib-ring-buffer.ko: >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-jbd2.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-jbd2.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-statedump.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-statedump.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-ext4.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-ext4.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-rcu.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-rcu.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-tracer.ko: armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-tracer.ko: armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-power.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-power.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko  >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-sched.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-sched.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
  
-sudo sh -c "echo armor/lttng-probe-block.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-block.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-jbd.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >>  $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-vmscan.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-vmscan.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-scsi.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-kprobes.ko: >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-regmap.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-scsi.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-skb.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-regmap.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-regulator.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-skb.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-random.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-ext3.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-workqueue.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
-sudo sh -c "echo armor/lttng-probe-regulator.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
-
-sudo sh -c "echo armor/lttng-probe-random.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
-
-sudo sh -c "echo armor/lttng-probe-workqueue.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
-
-sudo sh -c "echo armor/lttng-probe-irq.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-kprobes.ko armor/lttng-lib-ring-buffer.ko armor/lttng-kretprobes.ko >> $modules_dep_file"
+sudo sh -c "echo armor/lttng-probe-irq.ko: armor/lttng-tracer.ko armor/lttng-statedump.ko armor/lttng-ftrace.ko armor/lttng-lib-ring-buffer.ko >> $modules_dep_file"
 
 
 

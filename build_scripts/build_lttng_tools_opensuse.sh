4127721665c8e035746675517d3cd8d37f07f2c9
Shiju Jose <shiju.jose@huawei.com>
2016-04-15 10:19:08 +0100

    updated build_lttng_tools_opensuse.sh

--- build_scripts/build_lttng_tools_opensuse.sh
+++ build_scripts/build_lttng_tools_opensuse.sh
@@ -22,7 +22,7 @@ zypper install -y popt-devel
 zypper install -y libxml2-devel
 zypper install -y libuuid-devel
 zypper install -y liburcu-devel
-zypper install -y asciido
+zypper install -y asciidoc
 zypper install -y xmlto
 
 zypper install -y glib2-devel #(for babeltrace)

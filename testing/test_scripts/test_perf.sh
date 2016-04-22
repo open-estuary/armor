d159ac4cc08f56a325eea394a39a2f8f0d9046d8
Shiju Jose <shiju.jose@huawei.com>
2016-04-22 10:08:12 +0100

    updated test_perf.sh

--- testing/test_scripts/test_perf.sh
+++ testing/test_scripts/test_perf.sh
@@ -7,31 +7,8 @@
 
 ################################################################################
 #!/bin/bash
-    
-#perf
-Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 1`
-# Fix for OpenSuse distribution as the format of /etc/issue in OpenSuse is different.
-Distribution1=`sed -n 1p /etc/issue| cut -d' ' -f 3`
-if [ "$Distribution1" = 'openSUSE' ]; then
-    Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 3`
-fi
 
-case "$Distribution" in
-    Ubuntu)
-        PERF_BIN=/usr/lib/linux-tools-3.19.0-23/perf
-    ;;
-    Debian)
-        PERF_BIN=/usr/lib/linux-tools-3.19.0-23/perf
-    ;;
-    Fedora)
-        PERF_BIN=/usr/bin/perf
-    ;;
-    openSUSE)
-        PERF_BIN=/usr/bin/perf
-    ;;
-    esac
-
-${PERF_BIN} list | grep -i "hardware cache event"
+perf list | grep -i "hardware cache event"
 status=$?
 if test $status -eq 0
 then
@@ -41,7 +18,7 @@ else
     exit -1
 fi
 
-${PERF_BIN} stat -e r300 ls -l >/dev/null
+perf stat -e r24f300 ls -l >/dev/null
 status=$?
 if test $status -eq 0
 then

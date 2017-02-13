661d975dcb9e2aeadc20e2c5ee612c86b334339d
dikshit <dikshit.n@huawei.com>
2017-02-13 08:08:15 -0500

    armor: Updated test_perf.sh to support new version of perf
    Signed-off-by: dikshit <dikshit.n@huawei.com>

--- testing/test_scripts/test_perf.sh
+++ testing/test_scripts/test_perf.sh
@@ -26,7 +26,24 @@ else
     exit -1
 fi
 
-perf stat -e r24f300 ls -l >/dev/null
+perf stat -e cpu-cycles sleep 1 >/dev/null
+status=$?
+if test $status -eq 0
+then
+    echo "command perf cpu-cycles event [PASS]"
+else
+    echo "command perf cpu-cycles event [FAIL]"
+    exit -1
+fi
+
+perf_event_name=`perf list | grep "hisi_l3c" | head -n1 | awk -F '         ' '{print $1}'`
+if [ -z $perf_event_name ]
+then
+	echo "No hisi related perf events [FAIL]"
+	exit -1
+fi
+
+perf stat -a -e $perf_event_name sleep 1 >/dev/null
 status=$?
 if test $status -eq 0
 then
